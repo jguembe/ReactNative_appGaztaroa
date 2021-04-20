8bc45b3de6dcf5c7d4b1a4aea178b6ddd835f3b3
JGuembe <bitcreator@elude.in>
2021-04-20 16:01:25 +0200

    Componentes funcionales en React Native

--- componentes/CalendarioComponent.js
+++ componentes/CalendarioComponent.js
@@ -5,7 +5,10 @@ import { SafeAreaView, FlatList } from 'react-native';
 function Calendario(props) {
     const renderCalendarioItem = ({item, index}) => {
         return (
-            <ListItem key={index} bottomDivider>
+            <ListItem
+              key={index}
+              onPress={() => props.onPress(item.id)}
+              bottomDivider>
             <Avatar source={require('./imagenes/40Años.png')} />
             <ListItem.Content>
             <ListItem.Title>{item.nombre}</ListItem.Title>
