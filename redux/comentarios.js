8d55851b5ce8edaf621e468c5dfeaa3fee716e87
JGuembe <bitcreator@elude.in>
2021-05-23 20:32:40 +0200

    Firebase upload comentarios

--- redux/comentarios.js
+++ redux/comentarios.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { baseUrl } from '../comun/comun';
 
 export const comentarios = (state = { errMess: null, comentarios:[]}, action) => {
   switch (action.type) {
@@ -14,9 +15,23 @@ export const comentarios = (state = { errMess: null, comentarios:[]}, action) =>
         if (comentario.id> mayorid)
           mayorid = comentario.id;
       });
-      action.payload.id=mayorid+1;
+      mayorid++;
+      action.payload.id=mayorid;
+      let errmsg = null;
+      // Simple POST request with a JSON body using fetch
+      const requestOptions = {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify( action.payload )
+      };
+      fetch(baseUrl + 'comentarios/'+ mayorid + '.json', requestOptions)
+          .then(response => response.json())
+          .catch((error) => {
+            console.error(error);
+            errmsg = error.message;
+          });
 
-      return {...state, errMess: null, comentarios: state.comentarios.concat(action.payload)};
+      return {...state, errMess: errmsg, comentarios: state.comentarios.concat(action.payload)};
 
     default:
       return state;
