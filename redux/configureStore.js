3ab0dcc02022656330bfa70ace7b4391012a50ce
JGuembe <bitcreator@elude.in>
2021-06-07 21:31:55 +0200

    Memoria persistente

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from 'react-native';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
@@ -6,19 +7,21 @@ import { comentarios } from './comentarios';
 import { cabeceras } from './cabeceras';
 import { actividades } from './actividades';
 import { favoritos } from './favoritos';
+import { persistStore, persistReducer } from 'redux-persist';
 
-
-export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            excursiones,
-            comentarios,
-            cabeceras,
-            actividades,
-            favoritos
-        }),
-        applyMiddleware(thunk)
-    );
-
-    return store;
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
 }
+
+const rootReducer = combineReducers({
+    excursiones,
+    comentarios,
+    cabeceras,
+    actividades,
+    favoritos,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); // create a persisted reducer
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
--- redux/favoritos.js
+++ redux/favoritos.js
@@ -11,8 +11,11 @@ export const favoritos = (state = [], action) => {
         case ActionTypes.BORRAR_FAVORITO:
           if (state.some(el => el === action.payload)){
               var index = state.indexOf(action.payload);
-              if (index !== -1)
-                state.splice(index, 1);
+              if (index !== -1){
+                var newstate = [...state];
+                newstate.splice(index, 1);
+                return newstate;
+              }
           }
           return state;
 
